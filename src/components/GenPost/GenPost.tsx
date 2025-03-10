ae388219536ebf27b6d7f730e65e6a15f7d0477e
amitjoshi956 <amitjoshi956@gmail.com>
2025-03-10 18:32:14 +0000

    Code splitting and fixing API call

--- src/components/GenPost/GenPost.tsx
+++ src/components/GenPost/GenPost.tsx
@@ -1,64 +1,24 @@
-import { useState, ChangeEvent } from "react";
-import axios from "axios";
-import { getApiKey } from "@src/config";
+import { useState, ChangeEvent } from 'react';
+import { generatePost } from '@api/post';
 
-import "./GenPost.scss";
+import './GenPost.scss';
 
 const GenPost: React.FC = () => {
-  const [prompt, setPrompt] = useState<string>("");
-  const [generatedPost, setGeneratedPost] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>('');
+  const [generatedPost, setGeneratedPost] = useState<string>('');
 
   const handlePromptChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setPrompt(event.target.value);
   };
 
   const handleClearClick = () => {
-    setPrompt("");
+    setPrompt('');
   };
 
-  const generatePost = async () => {
-    if (!import.meta.env.VITE_API_KEY) {
-      console.error("VITE_API_KEY is not set. Please set it in the .env file.");
-      return;
-    }
-
-    const apiKey = getApiKey();
-
-    if (!apiKey) {
-      console.error(
-        "Error reading API Key, verify the .env file and config.ts file"
-      );
-    }
-
-    try {
-      const response = await axios.post(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" +
-          apiKey,
-        {
-          contents: [{ parts: [{ text: prompt }] }],
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (
-        response.data.candidates &&
-        response.data.candidates[0] &&
-        response.data.candidates[0].content &&
-        response.data.candidates[0].content.parts &&
-        response.data.candidates[0].content.parts[0]
-      ) {
-        const newPost = response.data.candidates[0].content.parts[0].text;
-        setGeneratedPost(newPost);
-      } else {
-        console.error(
-          "Error generating post: Response has not the expected structure"
-        );
-      }
-    } catch (error) {
-      console.error("Error generating post:", error);
+  const handleGenerateClick = async () => {
+    const newPost = await generatePost(prompt);
+    if (newPost) {
+      setGeneratedPost(newPost);
     }
   };
 
@@ -75,7 +35,10 @@ const GenPost: React.FC = () => {
           <button className="gen-post__clear-button" onClick={handleClearClick}>
             Clear
           </button>
-          <button className="gen-post__generate-button" onClick={generatePost}>
+          <button
+            className="gen-post__generate-button"
+            onClick={handleGenerateClick}
+          >
             Generate Post
           </button>
         </div>

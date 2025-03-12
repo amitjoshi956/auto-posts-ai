a76e4e24c83999f84e4d0a6c3027eff52e568890
amitjoshi956 <amitjoshi956@gmail.com>
2025-03-12 18:10:55 +0000

    Temporary disablement of GenPost component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.scss";
-import GenPost from "@components/GenPost";
+import './App.scss';
+import GenPost from '@components/GenPost';
 
 function App() {
   return (
     <div className="app">
       <h1 className="app-header">Auto Posts AI</h1>
-      <GenPost />
+      {/* Will add back later */}
+      {/* <GenPost /> */}
     </div>
   );
 }

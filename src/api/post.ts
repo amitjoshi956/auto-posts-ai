017a459d37da5a238e5d39ef6b3894a13897255e
amitjoshi956 <amitjoshi956@gmail.com>
2025-03-28 16:41:30 +0000

    Essentials from cloud-functions cleanup

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,5 +1,43 @@
 import axios from 'axios';
 
+export const generatePost = async (prompt: string): Promise<Post> => {
+  const apiKey = 'some-dummy-key'; // getGeminiApiKey();
+
+  if (!apiKey) {
+    console.error('Error reading API Key!');
+  }
+
+  try {
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+      {
+        contents: [{ parts: [{ text: prompt }] }],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    if (
+      response.data.candidates &&
+      response.data.candidates[0] &&
+      response.data.candidates[0].content &&
+      response.data.candidates[0].content.parts &&
+      response.data.candidates[0].content.parts[0]
+    ) {
+      const newPost = response.data.candidates[0].content.parts[0].text;
+      return newPost;
+    } else {
+      // logger.error('Error generating post: Response has not the expected structure');
+      return null;
+    }
+  } catch (error) {
+    // logger.error('Error generating post:', error);
+    return null;
+  }
+};
+
 export const fetchGeneratedPost = () => {
   return axios.get('/next-post').then((res) => res.data);
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -16,3 +16,37 @@ type APIResponse = {
   uid?: string;
   userName: string;
 };
+
+
+/*******************************
+ ******** Generic Types ********
+ *******************************/
+
+type GenericAPIResponse<D> = {
+  hasErrors?: boolean;
+  error?: string;
+  status: number;
+  data: D;
+};
+
+type GenericAPIError = {
+  hasErrors?: boolean;
+  status: number;
+  error: string;
+};
+
+type AuthRequest = {
+  email: string;
+  password: string;
+  displayName?: string;
+};
+
+type AuthResponse = {
+  idToken: string;
+  email: string;
+  displayName?: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+};

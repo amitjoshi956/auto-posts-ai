ce5da53cbad49ace16c15d349e733475adeeb2d6
Amit Joshi <amitjoshi956@gmail.com>
2025-03-31 16:26:26 +0530

    setup serverless for netlify

--- auto-posts-ai.core/src/index.ts
+++ auto-posts-ai.core/src/index.ts
@@ -1,18 +1,14 @@
 import express from 'express';
-import { getPort } from './config.js';
+import serverless from 'serverless-http';
+
 import authRoutes from './routes/auth.js';
 import postRoutes from './routes/post.js';
 
 const app = express();
-const port = getPort();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', postRoutes);
 app.use('/auth', authRoutes);
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-export default server;
+export const handler = serverless(app)
